dc2a26d01088ffe728784d38a913973d633885b4
Amit From VSCode <amitvashist7@example.com>
2023-01-26 19:07:53 +0530

    Calico has been removed form default install

--- 02-K8s/00-Setup/install-k8s-master-node.sh
+++ 02-K8s/00-Setup/install-k8s-master-node.sh
@@ -107,9 +107,9 @@ export KUBECONFIG=/etc/kubernetes/admin.conf
 sleep 1 
 
 echo "******************* Deploying kubernetes - Calico Network ***********************"
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/tigera-operator.yaml
-curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/custom-resources.yaml -O
-kubectl create -f custom-resources.yaml
+#kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/tigera-operator.yaml
+#curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/custom-resources.yaml -O
+#kubectl create -f custom-resources.yaml
 
 
 
